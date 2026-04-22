02e8aa6b36e7ed29d8d66593e00aabc8f70747f4
Vishal Singh <tvishalllodhi@gmail.com>
2026-04-22 18:41:34 +0530

    Initial commit

---
+++ themes/postertheme/assets/js/main/mainbase.js
@@ -0,0 +1,53 @@
+function toggleMenu(){
+    const nav = document.getElementById("navLinks");
+    nav.classList.toggle("active");
+
+    // ⭐ ADD
+    document.body.classList.toggle("menu-open");
+}
+
+function closeMenu(){
+    const nav = document.getElementById("navLinks");
+    nav.classList.remove("active");
+
+    // ⭐ ADD
+    document.body.classList.remove("menu-open");
+}
+
+/* Outside click close */
+document.addEventListener("click", function(event){
+    const nav = document.getElementById("navLinks");
+    const menuBtn = document.querySelector(".menu-btn");
+
+    if(!nav.contains(event.target) && !menuBtn.contains(event.target)){
+        nav.classList.remove("active");
+        document.body.classList.remove("menu-open"); // ⭐ ADD
+    }
+});
+
+function toggleDarkMode(){
+    document.body.classList.toggle("dark");
+    const btn = document.querySelector(".toggle-btn");
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("theme","dark");
+        btn.textContent="☀";
+    }else{
+        localStorage.setItem("theme","light");
+        btn.textContent="🌙";
+    }
+}
+
+if(localStorage.getItem("theme")==="dark"){
+    document.body.classList.add("dark");
+    document.querySelector(".toggle-btn").textContent="☀";
+}
+
+function searchTool(){
+    let input = document.getElementById("searchInput").value.toLowerCase();
+    let cards = document.querySelectorAll(".tool-card");
+
+    cards.forEach(card=>{
+        let title = card.querySelector("h3").textContent.toLowerCase();
+        card.style.display = title.includes(input) ? "block" : "none";
+    });
+}
